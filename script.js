f022557faff9ed789a822bd4ac79e4c6f37a1adc
Melissa <melissapotter.student@careerdevs.com>
2019-03-30 11:28:50 -0400

    using fetch to post data

---
+++ script.js
@@ -0,0 +1,14 @@
+var url = 'https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api';
+var data = {username: 'Melissa'};
+
+function postData(){
+fetch(url, {
+  method: 'POST', 
+  body: JSON.stringify(data), 
+  headers:{
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json())
+.then(response => console.log('Success:', JSON.stringify(response)))
+.catch(error => console.error('Error:', error));
+};

f4208f529b103c523c449af21d73c06b6b62da9f
Melissa <melissapotter.student@careerdevs.com>
2019-04-01 18:01:26 -0400

    added notes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url = 'https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api';
-var data = {username: 'Melissa', password: '1234'};
+const data = {username: 'Melissa', password: '1234'};
 
 function postData(){
 fetch(url, {
@@ -11,4 +11,21 @@ fetch(url, {
 }).then(res => res.json())
 .then(response => console.log('Success:', JSON.stringify(response)))
 .catch(error => console.error('Error:', error));
-};
+}
+
+// or 
+// .then(async function(response){
+//     const res = await response;
+//     console.log(res);
+//     });
+// }
+
+// aync promises
+
+// arrow function 
+// .then(response => console.log(response))
+
+// traditional 
+// .then(function(reponse){
+    
+// });
